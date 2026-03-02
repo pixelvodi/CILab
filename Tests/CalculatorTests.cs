05b9aafa6d5651f1cf72daa4719c62dc9ea402a2
Ваше Имя Фамилия <.email@example.com>
2026-03-02 15:26:32 +0300

    Инициализация проекта с модулями и тестами

---
+++ Tests/CalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using CalculatorModule;
+
+namespace Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ShouldReturnSum()
+        {
+            var calc = new Calculator();
+            var result = calc.Add(2, 3);
+            Assert.Equal(5, result);
+        }
+    }
+}
