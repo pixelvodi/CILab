a911a1c14f7fdce3b288d5ab5cd2806af75f9a6f
Ваше Имя Фамилия <.email@example.com>
2026-03-02 16:50:33 +0300

    Make test no fail intentionally

--- Tests/CalculatorTests.cs
+++ Tests/CalculatorTests.cs
@@ -18,7 +18,7 @@ namespace Tests
         {
             var calc = new Calculator();
             var result = calc.Add(2, 3);
-            Assert.Equal(6, result); // Неправильно, 2+3=5
+            Assert.Equal(5, result); // Неправильно, 2+3=5
         }
 
     }
